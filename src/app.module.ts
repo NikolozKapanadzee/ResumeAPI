5e5604bfc6c0e53477795093d1aa25b8fdbdf9ca
NikolozKapanadzee <nkapanadze369@gmail.com>
2025-11-28 02:04:31 +0400

    feat: created vacancies,applications and admin modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VacanciesModule } from './vacancies/vacancies.module';
+import { ApplicationsModule } from './applications/applications.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [VacanciesModule, ApplicationsModule, AdminModule],
 })
 export class AppModule {}
